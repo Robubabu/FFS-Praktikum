aa77c1cbfb23ffffa906e248511fb95dd45d8469
Pelle Ofenbach <pelle.ofenbach@tu-dortmund.de>
2016-12-11 16:39:46 +0100

    Plots 103 sowie korrigierter 355 bis auf Diskussion
    new file:   V103-Biegung_elastischer_Staebe/Plots/Reihe1.pdf
    new file:   V103-Biegung_elastischer_Staebe/Plots/Reihe2.pdf
    new file:   V103-Biegung_elastischer_Staebe/Plots/Reihe3.pdf
    new file:   V103-Biegung_elastischer_Staebe/Plots/reihe1.py
    new file:   V103-Biegung_elastischer_Staebe/Plots/reihe2.py
    new file:   V103-Biegung_elastischer_Staebe/Plots/reihe3.py
    new file:   V103-Biegung_elastischer_Staebe/Werte/V103-Reihe1.txt
    new file:   V103-Biegung_elastischer_Staebe/Werte/V103-Reihe2.txt
    new file:   V103-Biegung_elastischer_Staebe/Werte/V103-Reihe3.txt
    modified:   V103-Biegung_elastischer_Staebe/content/auswertung.tex
    modified:   V355-Gekoppelte_Schwingungen/content/auswertung.tex
    modified:   V355-Gekoppelte_Schwingungen/content/durchfuehrung.tex
    modified:   V355-Gekoppelte_Schwingungen/content/theorie.tex

---
+++ V103-Biegung_elastischer_Staebe/Plots/reihe2.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import uncertainties.unumpy as unp
+from scipy.optimize import curve_fit
+
+x,y0,y1 = np.genfromtxt('../Werte/V103-Reihe2.txt', unpack = True)
+
+plt.plot(x,y0, 'ko', label = 'Unbelastet')
+plt.plot(x,y1, 'ro', label = 'Belastet')
+plt.plot(x,y1-y0, 'go', label = 'Differenz')
+
+plt.xlabel(r'$X \:/\: cm$')
+plt.ylabel(r'$y \:/\: mm$')
+plt.legend(loc='best')
+
+plt.savefig('Reihe2.pdf')
