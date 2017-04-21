4ae8edca14b711664fb25f291a13421c039f9b72
Robubabu <robert.appel@udo.edu>
2017-04-21 13:07:56 +0200

    auswertung 2.0 601

--- V601-Franck-Hertz/plots/8a1.py
+++ V601-Franck-Hertz/plots/8a1.py
@@ -10,19 +10,25 @@ a,p = np.genfromtxt('8a1-Werte.txt' , unpack = True)
 sx = np.genfromtxt('8a1-Skalarx.txt', unpack = True)
 def mittel(x):              #the real mean()-ing of life
     return ufloat(np.mean(x),np.std(x,ddof=1)/np.sqrt(len(x)))
-
+a_u = a
+p_u = p
 sx = mittel(sx)
 print('Mittlere x Skalen Abstand:' , sx)
 sy = 4.6 /12.3 # nano A duch cm
+print('y-Achsen Referenzwert:', sy)
 f = sy/sx*10**-9 # Skalenfaktor A / V
-print(f)
+print('Umrechnungsfaktor:', f)
 a = a*f
 p = p/sx
+m = p[10:13]
+M = np.mean(m)
+print(M)
 #plt.subplot(1, 2, 1)
+np.savetxt('steigung.txt',np.column_stack([a_u,p_u,noms(a),stds(a),noms(p),stds(p)]), delimiter=' & ',newline= r'\\'+'\n' )
 plt.plot(noms(p), noms(a), 'rx' ,label='Differentielle Energieverteilung')
-plt.axvline(x=10,)
+plt.axvline(x=noms(M))
 plt.xlabel(r'$U_A / V$')
 plt.ylabel(r'$ \frac{\Delta I_A}{\Delta U_A} / \frac{A}{V}$')
 plt.legend(loc='best')
-plt.show()
-# plt.savefig('build/plot2.pdf')
+# plt.show()
+plt.savefig('8a1plot.pdf')
