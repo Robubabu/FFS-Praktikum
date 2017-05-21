f1c9efa1d32a05aac0ad51fec61cd2d8f6c9b7f8
Pelle Ofenbach <pelle.ofenbach@tu-dortmund.de>
2017-05-21 10:30:21 +0200

    Rechtschreib-Korrektur+Histogramm für Statistik (nicht alle Werte) 	modified:   V701-Reichweite_von_Alphastrahlung/Makefile 	new file:   V701-Reichweite_von_Alphastrahlung/Messwerte/Statistik.txt 	modified:   V701-Reichweite_von_Alphastrahlung/content/auswertung.tex 	modified:   V701-Reichweite_von_Alphastrahlung/content/durchfuehrung.tex 	modified:   V701-Reichweite_von_Alphastrahlung/content/theorie.tex 	new file:   V701-Reichweite_von_Alphastrahlung/plots/Statistik.pdf 	modified:   V701-Reichweite_von_Alphastrahlung/plots/plot.py

--- V701-Reichweite_von_Alphastrahlung/plots/plot.py
+++ V701-Reichweite_von_Alphastrahlung/plots/plot.py
@@ -5,32 +5,15 @@ from scipy.optimize import curve_fit
 from uncertainties import correlated_values, correlation_matrix
 from uncertainties import ufloat
 from uncertainties.unumpy import (nominal_values as noms, std_devs as stds)
-x = np.linspace(0, 10, 1000)
-def mittel(x):              #the real mean()-ing of life
-    return ufloat(np.mean(x),np.std(x,ddof=1)/np.sqrt(len(x)))
-
-#Fit
-params , cov = curve_fit(f , x ,y )
-params = correlated_values(params, cov)
-for p in params:
-    print(p)
 
 
-#Tabelle 
+#Tabelle
 # np.savetxt('tab.txt',np.column_stack([x,y]), delimiter=' & ',newline= r'\\'+'\n' )
 #plt.subplot(1, 2, 1)
-plt.plot(x, y, label='Kurve')
-plt.xlabel(r'$\alpha \:/\: \si{\ohm}$')
-plt.ylabel(r'$y \:/\: \si{\micro\joule}$')
-plt.legend(loc='best')
-plt.savefig('build/plot.pdf')
-plt.clf()
-#plt.subplot(1, 2, 2)
-plt.plot(x, y, label='Kurve')
-plt.xlabel(r'$\alpha \:/\: \si{\ohm}$')
-plt.ylabel(r'$y \:/\: \si{\micro\joule}$')
-plt.legend(loc='best')
 
-# in matplotlibrc leider (noch) nicht möglich
-#plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
-plt.savefig('build/plot2.pdf')
+x=np.genfromtxt('../Messwerte/Statistik.txt')
+plt.hist(x)
+plt.xlabel('Impacts/s')
+
+plt.savefig('Statistik.pdf')
+plt.clf()
