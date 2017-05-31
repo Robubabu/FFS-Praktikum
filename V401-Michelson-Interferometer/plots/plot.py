0dfcc74a24b5a3a063541681438d835a30926a13
Pelle Ofenbach <pelle.ofenbach@tu-dortmund.de>
2017-05-31 12:02:48 +0200

    Titelbild aktualisiert 	modified:   content/auswertung.tex 	modified:   content/diskussion.tex 	modified:   lit.bib 	modified:   main.tex 	new file:   plots/Gas.txt 	new file:   plots/Luft.txt 	new file:   plots/N_tab.txt 	new file:   plots/lambda.txt 	new file:   plots/lambda_tab.txt 	modified:   plots/plot.py

--- V401-Michelson-Interferometer/plots/plot.py
+++ V401-Michelson-Interferometer/plots/plot.py
@@ -7,30 +7,70 @@ from uncertainties import ufloat
 from uncertainties.unumpy import (nominal_values as noms, std_devs as stds)
 x = np.linspace(0, 10, 1000)
 def mittel(x):              #the real mean()-ing of life
-    return ufloat(np.mean(x),np.std(x,ddof=1)/np.sqrt(len(x)))
+    return ufloat(np.mean(x),np.std(x,ddof=1)/unp.sqrt(len(x)))
 
 #Fit
-params , cov = curve_fit(f , x ,y )
-params = correlated_values(params, cov)
-for p in params:
-    print(p)
+#params , cov = curve_fit(f , x ,y )
+#params = correlated_values(params, cov)
+#for p in params:
+#    print(p)
 
 
-#Tabelle 
+#Tabelle
 # np.savetxt('tab.txt',np.column_stack([x,y]), delimiter=' & ',newline= r'\\'+'\n' )
 #plt.subplot(1, 2, 1)
-plt.plot(x, y, label='Kurve')
-plt.xlabel(r'$\alpha \:/\: \si{\ohm}$')
-plt.ylabel(r'$y \:/\: \si{\micro\joule}$')
-plt.legend(loc='best')
-plt.savefig('build/plot.pdf')
-plt.clf()
-#plt.subplot(1, 2, 2)
-plt.plot(x, y, label='Kurve')
-plt.xlabel(r'$\alpha \:/\: \si{\ohm}$')
-plt.ylabel(r'$y \:/\: \si{\micro\joule}$')
-plt.legend(loc='best')
 
-# in matplotlibrc leider (noch) nicht möglich
-#plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
-plt.savefig('build/plot2.pdf')
+####################################################################
+#########           WAVELENGTH        ##############################
+####################################################################
+
+startCount, endCount, startX, endX = np.genfromtxt('lambda.txt', unpack = True)
+x = endX - startX
+x = x*10**6 #x in nm
+j = np.linspace(0,len(x)-1, len(x))
+for i in j:
+    if(x[i]<0):
+        x[i] = -1*x[i]
+count = endCount - startCount
+
+
+length = 2*x/(count)/5.017 #lever
+print('wavelength: ', length)
+
+middleLength = mittel(length)
+print('middleLength: ', middleLength)
+
+
+np.savetxt('lambda_tab.txt',np.column_stack([count, x, length]), delimiter=' & ',newline= r'\\'+'\n' )
+
+#########################################################################
+##################### BREAKING BAD ######################################
+#########################################################################
+
+
+startCountAir, endCountAir = np.genfromtxt('Luft.txt', unpack = True)
+countAir = endCountAir - startCountAir
+pAir = 0.8
+pGas, startCountGas, endCountGas = np.genfromtxt('Gas.txt', unpack = True)
+countGas = endCountGas-startCountGas
+
+dnAir = countAir * middleLength / (2*50*10**6)
+dnGas = countGas * middleLength / (2*50*10**6)
+
+dnAir0 = dnAir * 298.15/273.15*1.0132/pAir
+dnGas0 = dnGas * 298.15/273.15*1.0132/pGas
+
+np.savetxt('N_tab.txt',np.column_stack([countAir,  noms(dnAir), stds(dnAir), pGas, countGas, noms(dnGas), stds(dnGas)]), delimiter=' & ',newline= r'\\'+'\n' )
+print(len(countAir), len(dnAir), len(pGas), len(countGas), len(dnGas))
+print('Delta N Air: ' , mittel(noms(dnAir0)))
+print('Delta N Gas: ' , mittel(noms(dnGas0)))
+#plt.plot(x, y, label='Kurve')
+#plt.xlabel(r'$\alpha \:/\: \si{\ohm}$')
+#plt.ylabel(r'$y \:/\: \si{\micro\joule}$')
+#plt.legend(loc='best')
+#plt.savefig('build/Wavelength.pdf')
+#plt.clf()
+
+####################################################################
+####################################################################
+####################################################################
