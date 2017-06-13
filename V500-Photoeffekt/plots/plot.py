7b22b1d6a2e6387711f2c5f5bbe796d14ed4a2b9
Pelle Ofenbach <pelle.ofenbach@tu-dortmund.de>
2017-06-13 09:36:27 +0200

    V500 Erste Abgabe Auswertung 	renamed:    V401-Michelson-Interferometer/SHK17.pdf.pdf -> V401-Michelson-Interferometer/Werte.pdf 	modified:   V500-Photoeffekt/content/auswertung.tex 	modified:   V500-Photoeffekt/content/diskussion.tex 	new file:   V500-Photoeffekt/plots/Orange.pdf 	new file:   V500-Photoeffekt/plots/Plot.pdf 	new file:   V500-Photoeffekt/plots/Strom.pdf 	new file:   V500-Photoeffekt/plots/blau1.txt 	new file:   V500-Photoeffekt/plots/blau1_tab.txt 	new file:   V500-Photoeffekt/plots/blau2.txt 	new file:   V500-Photoeffekt/plots/blau2_tab.txt 	new file:   V500-Photoeffekt/plots/fine.txt 	new file:   V500-Photoeffekt/plots/fine_tab.txt 	new file:   V500-Photoeffekt/plots/gruen.txt 	new file:   V500-Photoeffekt/plots/gruen_tab.txt 	new file:   V500-Photoeffekt/plots/h.pdf 	new file:   V500-Photoeffekt/plots/orange.txt 	new file:   V500-Photoeffekt/plots/orange_tab.txt 	modified:   V500-Photoeffekt/plots/plot.py

--- V500-Photoeffekt/plots/plot.py
+++ V500-Photoeffekt/plots/plot.py
@@ -1,36 +1,125 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import uncertainties.unumpy as unp
+import scipy
 from scipy.optimize import curve_fit
 from uncertainties import correlated_values, correlation_matrix
 from uncertainties import ufloat
 from uncertainties.unumpy import (nominal_values as noms, std_devs as stds)
-x = np.linspace(0, 10, 1000)
+#x = np.linspace(0, 10, 1000)
 def mittel(x):              #the real mean()-ing of life
     return ufloat(np.mean(x),np.std(x,ddof=1)/np.sqrt(len(x)))
 
+def f(x,a,b):
+    return a*x+b
+
 #Fit
-params , cov = curve_fit(f , x ,y )
-params = correlated_values(params, cov)
-for p in params:
-    print(p)
+#params , cov = curve_fit(f , x ,y )
+#params = correlated_values(params, cov)
+#for p in params:
+#    print(p)
 
+u, i= np.genfromtxt('./fine.txt', unpack = True)
+u =-u
+#Tabelle
+np.savetxt('fine_tab.txt',np.column_stack([u,i]), delimiter=' & ',newline= r'\\'+'\n' )
 
-#Tabelle 
-# np.savetxt('tab.txt',np.column_stack([x,y]), delimiter=' & ',newline= r'\\'+'\n' )
-#plt.subplot(1, 2, 1)
-plt.plot(x, y, label='Kurve')
-plt.xlabel(r'$\alpha \:/\: \si{\ohm}$')
-plt.ylabel(r'$y \:/\: \si{\micro\joule}$')
+plt.plot(u, i, 'kx', label='Oranges Licht')
+
+plt.xlabel(r'$U / V$')
+plt.ylabel(r'$\sqrt{I / nA}$')
 plt.legend(loc='best')
-plt.savefig('build/plot.pdf')
+plt.savefig('Orange.pdf')
 plt.clf()
-#plt.subplot(1, 2, 2)
-plt.plot(x, y, label='Kurve')
-plt.xlabel(r'$\alpha \:/\: \si{\ohm}$')
-plt.ylabel(r'$y \:/\: \si{\micro\joule}$')
+
+u, i= np.genfromtxt('./orange.txt', unpack = True)
+u =-u
+#Tabelle
+np.savetxt('orange_tab.txt',np.column_stack([u,i]), delimiter=' & ',newline= r'\\'+'\n' )
+
+params, cov = curve_fit(f,u[19:29], np.sqrt(i[19:29]))
+error = np.sqrt(np.diag(cov))
+
+x = np.linspace(-0.4,0)
+print('Orange: ', params, error)
+plt.subplot(2,2, 1)
+plt.plot(u, np.sqrt(i), 'kx', label='Oranges Licht')
+plt.plot(x, f(x,*params), 'k--', label = 'fit')
+plt.xlabel(r'$U / V$')
+plt.ylabel(r'$\sqrt{I / nA}$')
 plt.legend(loc='best')
 
-# in matplotlibrc leider (noch) nicht möglich
-#plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
-plt.savefig('build/plot2.pdf')
+u, i= np.genfromtxt('./gruen.txt', unpack = True)
+u = -u
+#Tabelle
+np.savetxt('gruen_tab.txt',np.column_stack([u,i]), delimiter=' & ',newline= r'\\'+'\n' )
+
+params, cov = curve_fit(f,u[22:32], np.sqrt(i[22:32]))
+error = np.sqrt(np.diag(cov))
+
+x = np.linspace(-0.5,0)
+print('Gruen: ', params, error)
+
+plt.subplot(2, 2, 2)
+plt.plot(u, np.sqrt(i), 'kx', label='Grünes Licht')
+plt.plot(x, f(x,*params), 'k--', label = 'fit')
+plt.xlabel(r'$U / V$')
+plt.ylabel(r'$\sqrt{I / nA}$')
+plt.legend(loc='best')
+
+u, i= np.genfromtxt('./blau1.txt', unpack = True)
+u = -u
+#Tabelle
+np.savetxt('blau1_tab.txt',np.column_stack([u,i]), delimiter=' & ',newline= r'\\'+'\n' )
+
+params, cov = curve_fit(f,u[22:32], np.sqrt(i[22:32]))
+error = np.sqrt(np.diag(cov))
+
+x = np.linspace(-1.0,0)
+print('Blau1: ', params, error)
+
+plt.subplot(2,2, 3)
+plt.plot(u, np.sqrt(i), 'kx', label='Blaues Licht')
+plt.plot(x, f(x,*params), 'k--', label = 'fit')
+plt.xlabel(r'$U / V$')
+plt.ylabel(r'$\sqrt{I / nA}$')
+plt.legend(loc='best')
+
+u, i= np.genfromtxt('./blau2.txt', unpack = True)
+u = -u
+#Tabelle
+np.savetxt('blau2_tab.txt',np.column_stack([u,i]), delimiter=' & ',newline= r'\\'+'\n' )
+
+params, cov = curve_fit(f,u[27:33], np.sqrt(i[27:33]))
+error = np.sqrt(np.diag(cov))
+
+x = np.linspace(-1.2,0)
+print('Blau2: ', params, error)
+
+plt.subplot(2,2, 4)
+plt.plot(u, np.sqrt(i), 'kx', label='Blaueres Licht')
+plt.plot(x, f(x,*params), 'k--', label = 'fit')
+plt.xlabel(r'$U / V$')
+plt.ylabel(r'$\sqrt{I / nA}$')
+plt.legend(loc='best')
+
+
+
+plt.savefig('Strom.pdf')
+plt.clf()
+
+c = 3*10**8
+ug = unp.uarray([0.45,0.53,1.01,1.18],[0.02,0.02,0.03,0.05])
+v = np.array([c/578.0, c/546.0, c/492.0, c/435.0])
+v = (1/1000)*v
+
+params,cov = curve_fit(f,v,noms(ug))
+error = np.sqrt(np.diag(cov))
+print('h: ', params, error)
+x = np.linspace(400,700)
+plt.errorbar(v, noms(ug), yerr = stds(ug), fmt = 'kx', label = 'Grenzspannungen')
+plt.plot(x, f(x,*params),'k--', label = 'fit')
+plt.xlabel('f/kHz')
+plt.ylabel('U/V')
+plt.legend(loc = 'best')
+plt.savefig('h.pdf')
