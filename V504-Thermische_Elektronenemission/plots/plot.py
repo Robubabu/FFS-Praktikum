d70c67eab286827f7cb8adee2aa875ff965a2ae3
Pelle Ofenbach <pelle.ofenbach@tu-dortmund.de>
2017-06-27 09:26:18 +0200

    V504 	modified:   V504-Thermische_Elektronenemission/content/auswertung.tex 	modified:   V504-Thermische_Elektronenemission/content/diskussion.tex 	new file:   V504-Thermische_Elektronenemission/plots/Plot1.pdf 	new file:   V504-Thermische_Elektronenemission/plots/Plot2.pdf 	new file:   V504-Thermische_Elektronenemission/plots/Plot3.pdf 	new file:   V504-Thermische_Elektronenemission/plots/Plot4.pdf new file:   V504-Thermische_Elektronenemission/plots/Plot5.pdf 	new file:   V504-Thermische_Elektronenemission/plots/Plot6.pdf 	new file:   V504-Thermische_Elektronenemission/plots/Reihe1.txt 	new file:   V504-Thermische_Elektronenemission/plots/Reihe1_tab.txt 	new file:   V504-Thermische_Elektronenemission/plots/Reihe2.txt 	new file:   V504-Thermische_Elektronenemission/plots/Reihe2_tab.txt 	new file:   V504-Thermische_Elektronenemission/plots/Reihe3.txt 	new file:   V504-Thermische_Elektronenemission/plots/Reihe3_tab.txt 	new file:   V504-Thermische_Elektronenemission/plots/Reihe4.txt 	new file:   V504-Thermische_Elektronenemission/plots/Reihe4_tab.txt 	new file:   V504-Thermische_Elektronenemission/plots/Reihe5.txt 	new file:   V504-Thermische_Elektronenemission/plots/Reihe5_tab.txt 	new file:   V504-Thermische_Elektronenemission/plots/Reihe6.txt 	new file:   V504-Thermische_Elektronenemission/plots/Reihe6_tab.txt 	new file:   V504-Thermische_Elektronenemission/plots/langmuir.pdf 	modified:   V504-Thermische_Elektronenemission/plots/plot.py

--- V504-Thermische_Elektronenemission/plots/plot.py
+++ V504-Thermische_Elektronenemission/plots/plot.py
@@ -5,32 +5,114 @@ from scipy.optimize import curve_fit
 from uncertainties import correlated_values, correlation_matrix
 from uncertainties import ufloat
 from uncertainties.unumpy import (nominal_values as noms, std_devs as stds)
-x = np.linspace(0, 10, 1000)
+
 def mittel(x):              #the real mean()-ing of life
     return ufloat(np.mean(x),np.std(x,ddof=1)/np.sqrt(len(x)))
 
-#Fit
-params , cov = curve_fit(f , x ,y )
-params = correlated_values(params, cov)
-for p in params:
-    print(p)
 
-
-#Tabelle 
-# np.savetxt('tab.txt',np.column_stack([x,y]), delimiter=' & ',newline= r'\\'+'\n' )
-#plt.subplot(1, 2, 1)
-plt.plot(x, y, label='Kurve')
-plt.xlabel(r'$\alpha \:/\: \si{\ohm}$')
-plt.ylabel(r'$y \:/\: \si{\micro\joule}$')
-plt.legend(loc='best')
-plt.savefig('build/plot.pdf')
+U, I = np.genfromtxt('./Reihe1.txt', unpack = True)
+np.savetxt('Reihe1_tab.txt', np.column_stack([U,I]), delimiter = ' & ',newline= r'\\'+'\n' )
+plt.plot(U, I, 'k.')
+plt.ylabel(r'$I/mA$')
+plt.xlabel(r'$U/V$')
+plt.xlim(-1,200)
+plt.ylim(-0.001,0.013)
+plt.grid()
+plt.savefig('Plot1.pdf')
 plt.clf()
-#plt.subplot(1, 2, 2)
-plt.plot(x, y, label='Kurve')
-plt.xlabel(r'$\alpha \:/\: \si{\ohm}$')
-plt.ylabel(r'$y \:/\: \si{\micro\joule}$')
-plt.legend(loc='best')
 
-# in matplotlibrc leider (noch) nicht möglich
-#plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
-plt.savefig('build/plot2.pdf')
+U, I = np.genfromtxt('./Reihe2.txt', unpack = True)
+np.savetxt('Reihe2_tab.txt', np.column_stack([U,I]), delimiter = ' & ',newline= r'\\'+'\n' )
+plt.plot(U, I, 'k.')
+plt.ylabel(r'$I/mA$')
+plt.xlabel(r'$U/V$')
+plt.ylim(-0.001,0.025)
+plt.grid()
+plt.savefig('Plot2.pdf')
+plt.clf()
+
+U, I = np.genfromtxt('./Reihe3.txt', unpack = True)
+np.savetxt('Reihe3_tab.txt', np.column_stack([U,I]), delimiter = ' & ',newline= r'\\'+'\n' )
+plt.plot(U, I, 'k.')
+plt.ylabel(r'$I/mA$')
+plt.xlabel(r'$U/V$')
+plt.xlim(-1,251)
+plt.grid()
+plt.savefig('Plot3.pdf')
+plt.clf()
+
+U, I = np.genfromtxt('./Reihe4.txt', unpack = True)
+np.savetxt('Reihe4_tab.txt', np.column_stack([U,I]), delimiter = ' & ',newline= r'\\'+'\n' )
+plt.plot(U, I, 'k.')
+plt.ylabel(r'$I/mA$')
+plt.xlabel(r'$U/V$')
+plt.xlim(-1, 101)
+plt.ylim(-0.001,0.16)
+plt.grid()
+plt.savefig('Plot4.pdf')
+plt.clf()
+
+U, I = np.genfromtxt('./Reihe5.txt', unpack = True)
+np.savetxt('Reihe5_tab.txt', np.column_stack([U,I]), delimiter = ' & ',newline= r'\\'+'\n' )
+plt.plot(U, I, 'k.')
+plt.ylabel(r'$I/mA$')
+plt.xlabel(r'$U/V$')
+plt.xlim(-1,251)
+plt.grid()
+plt.savefig('Plot5.pdf')
+plt.clf()
+
+U = np.log(U)
+I = np.log(I)
+
+def g(x,a,b):
+    return a*x+b
+
+params, cov = curve_fit(g,U[0:6], I[0:6])
+error = np.sqrt(np.diag(cov))
+print('P: ', params, ' +/- ', error)
+
+plt.plot(U[0:6], g(U[0:6], *params), 'k-', label = 'Ausgleichsgerade')
+plt.plot(U, I, 'k.', label = 'Messwerte')
+plt.ylabel(r'$ln(I/mA)$')
+plt.xlabel(r'$ln(U/V)$')
+plt.grid()
+plt.legend(loc = 'best')
+plt.savefig('langmuir.pdf')
+plt.clf()
+
+
+U, I = np.genfromtxt('Reihe6.txt', unpack = True)
+np.savetxt('Reihe6_tab.txt', np.column_stack([U,I]), delimiter = ' & ',newline= r'\\'+'\n' )
+params, cov = curve_fit(g,U[0:9], np.log(I[0:9]))
+error = np.sqrt(np.diag(cov))
+a = ufloat(1.9,0.1)
+k = 8.6e-5
+e = 1.6e-19
+T = 1/(k*a)
+print(U, T)
+
+print('Anlauf: ', params, '+/-', error)
+plt.plot(U, np.log(I), 'k.', label = 'Messwerte')
+plt.plot(U[0:9], g(U[0:9], *params), 'k-', label = 'Ausgleichsgerade')
+plt.ylabel(r'$ln(I/nA)$')
+plt.xlabel(r'$U/V$')
+plt.legend(loc = 'best')
+plt.grid()
+plt.savefig('Plot6.pdf')
+plt.clf()
+T = ufloat(6120, 320)
+f = 0.35
+n = 0.28
+s = 5.7e-12
+I = np.array([1.8,1.9,2.0,2.1])
+U = np.array([3.5,4,4.5,4.5])
+N = 1
+h = 4.14e-15
+m = 9.1e-31
+print('N: ', N)
+T = ((I*U-N)/(f*n*s))**0.25
+print('T: ', T)
+P = k*T*np.log(I*h**3/(4*np.pi*e*m*k**2*T**2))
+print('Phi: ', P)
+print('mittel: ', mittel(P))
