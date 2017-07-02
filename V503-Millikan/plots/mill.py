8f1e2001339b2dfa20a745a0f1350cd4abf3b900
Robubabu <robert.appel@udo.edu>
2017-07-02 12:16:50 +0200

    Commitung zu Millikan

--- V503-Millikan/plots/mill.py
+++ V503-Millikan/plots/mill.py
@@ -17,9 +17,9 @@ def mittel(x):              #the real mean()-ing of life
 def g(x,a,b):
     return a*x+b
 def rkorr(n,v):
-    return unp.sqrt((B/(2*p))+((9*n*v)/(2*const.g*rho)))-(B/(2*p))
+    return unp.sqrt(((B/(2*p))**2)+((9*n*v)/(2*const.g*rho)))-(B/(2*p))
 def q(r,E):
-    return ((4*np.pi/3)*rho*const.g*r**3)/E
+    return ((4*np.pi/3)*rho*const.g*(r**3))/E
 
 R, t, U = np.genfromtxt('millikan.txt', unpack = True)
 # R thermo wiederstand in Mega Ohm
@@ -39,7 +39,6 @@ for r in R:
 E = np.absolute(U / d )# EFeld  im Kondensator berechnen
 v = s/t # v null  Geschindigkeit ohne Feld
 
-
 nT = np.array([16,17,18])
 nn = np.array([1.805,1.81,1.814])
 #Fit
@@ -49,19 +48,38 @@ na,nb = nparams
 n = np.array(g(T,na,nb)) #viskositäts array
 n*=10**-5
 Q = q(rkorr(n,v),E)
-eQ = np.sort(Q/const.e)
 er = np.sort(rkorr(n,v))
 Q = np.sort(Q)
+# Niveaus Bestimmen
+q0 = np.array([])
+q1 = np.array([])
+q2 = np.array([])
+for q in Q:
+    if q < 5e-20:
+        q0 = np.append(q0,q)
+    if q > 1.5e-19 and q < 2e-19:
+        q1 = np.append(q1,q)
+    if q > 3.5e-19 and q < 4e-19:
+        q2 = np.append(q1,q)
+q0 = np.mean(q0)
+q1 = np.mean(q1)
+q2 = np.mean(q2)
+print('mittelwert von q1:',q1)
+print('Mittelwert von q2:',q2)
+print('Berechnete Elementarladung:',(q1 + (1/2)*q2)/2)
 #Tabelle
-np.savetxt('millitab.txt',np.column_stack([(noms(v)),noms(T),noms(n)*10**5,noms(rkorr(n,v)),stds(rkorr(n,v)),noms(E)]), delimiter=' & ',newline= r'\\'+'\n' )
-#plt.subplot(1, 2, 1)
-plt.plot(noms(er), noms(Q),'ro', label='Kurve')
+# np.savetxt('millitab.txt',np.column_stack([(noms(v)),noms(T),noms(n)*10**5,noms(rkorr(n,v)),stds(rkorr(n,v)),noms(E)]), delimiter=' & ',newline= r'\\'+'\n' )
+# #plt.subplot(1, 2, 1)
+plt.plot(noms(er), noms(Q),'ro', label='Ladung')
+# plt.axhline(y=4.5e-20 , color='g' , linestyle='--', label='Niveau 0')
+plt.axhline(y=1.8e-19 , color='g' , linestyle='--', label='Niveau 1')
+plt.axhline(y=3.8e-19 , color='b' , linestyle='--', label='Niveau 2')
 plt.xlabel(r'$r / m$')
 plt.ylabel(r'$Q / C$')
 plt.grid()
-# plt.legend(loc='best')
-plt.show()
-# plt.savefig('milliplot.pdf')
+plt.legend(loc='best')
+# plt.show()
+plt.savefig('milliplot.pdf')
 # plt.clf()
 # #plt.subplot(1, 2, 2)
 # plt.plot(x, y, label='Kurve')
